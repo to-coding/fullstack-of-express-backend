687b1c4032884355cda950a7bde72551b3fdaafd
gaoxiang.huang <hgx1233626@163.com>
2023-04-24 17:07:36 +0800

    feat: add post request

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const { request, response } = require('express')
 const express = require('express')
 const app = express()
+app.use(express.json())
+
+
 let notes = [
     {
         id: 1,
@@ -22,6 +25,32 @@ let notes = [
     }
 ]
 
+const generateId = () => {
+    const maxId = notes.length > 0 ? Math.max(...notes.map(n => n.id)) : 0
+    return maxId + 1
+}
+
+app.post('/api/notes', (request, response) => {
+    const body = request.body
+
+    if (!body.content) {
+        return response.status(400).json({
+            error: 'content missing'
+        })
+    }
+
+    const note = {
+        content: body.content,
+        important: body.important || false,
+        date: new Date(),
+        id: generateId(),
+    }
+
+    notes = notes.concat(note)
+
+    response.json(note)
+})
+
 app.get('/', (request, response) => {
     response.send('<h1>Hello World</h1>')
 })
@@ -40,7 +69,7 @@ app.get('/api/notes/:id', (request, response) => {
     }
 })
 
-app.delete('/api/notes/:id', (request, response)=>{
+app.delete('/api/notes/:id', (request, response) => {
     const id = Number(request.params.id)
     notes = notes.filter(n => n.id !== id)
 
