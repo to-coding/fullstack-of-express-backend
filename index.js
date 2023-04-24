eefe10ff44012cba5d580d37ef69f7131d1902b0
gaoxiang.huang <hgx1233626@163.com>
2023-04-24 17:17:53 +0800

    use Middleware

--- index.js
+++ index.js
@@ -1,7 +1,20 @@
 const { request, response } = require('express')
 const express = require('express')
 const app = express()
+const requestLogger = (request, response, next) => {
+    console.log('Method:', request.method)
+    console.log('Path:', request.path)
+    console.log('Body:', request.body)
+    console.log('---')
+    next()
+}
+const unknownEndpoint = (request, response) => {
+    response.status(404).send({error: 'unknown endpoint'})
+}
+
 app.use(express.json())
+app.use(requestLogger)
+
 
 
 let notes = [
@@ -76,6 +89,7 @@ app.delete('/api/notes/:id', (request, response) => {
     response.status(204).end()
 })
 
+app.use(unknownEndpoint)
 const PORT = 3002
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)
