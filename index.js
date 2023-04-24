3a2ee181c8b81d01a68c84a1fccba9c70dcb6fad
gaoxiang.huang <hgx1233626@163.com>
2023-04-24 16:46:47 +0800

    feat: add api

--- index.js
+++ index.js
@@ -29,6 +29,23 @@ app.get('/', (request, response) => {
 app.get('/api/notes', (request, response) => {
     response.json(notes)
 })
+app.get('/api/notes/:id', (request, response) => {
+    const id = Number(request.params.id)
+    const note = notes.find(note => note.id === id)
+
+    if (note) {
+        response.json(note)
+    } else {
+        response.status(404).end()
+    }
+})
+
+app.delete('/api/notes/:id', (request, response)=>{
+    const id = Number(request.params.id)
+    notes = notes.filter(n => n.id !== id)
+
+    response.status(204).end()
+})
 
 const PORT = 3002
 app.listen(PORT, () => {
