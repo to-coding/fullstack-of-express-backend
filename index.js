dfddd873bbd42994e926697f307b4b314e350cef
gaoxiang.huang <hgx1233626@163.com>
2023-04-25 16:20:26 +0800

    refactor: extract mongodb as model

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
-const { request, response } = require('express')
+// config env variable
+require('dotenv').config();
+const Note = require('./models/note')
+const PORT = process.env.PORT
 
 // express backend
+const { request, response } = require('express')
 const express = require('express')
 const app = express()
 const requestLogger = (request, response, next) => {
@@ -13,64 +17,38 @@ const requestLogger = (request, response, next) => {
 const unknownEndpoint = (request, response) => {
     response.status(404).send({ error: 'unknown endpoint' })
 }
+app.use(express.json())
+app.use(requestLogger)
+app.use(express.static('build'))
 
 // allow cors
 const cors = require('cors')
-app.use(express.json())
-app.use(requestLogger)
 app.use(cors())
-app.use(express.static('build'))
-
-// config mongo db
-const mongoose = require('mongoose')
-const url = `mongodb+srv://sece1024:sece@cluster0.5mrpqe5.mongodb.net/noteApp?retryWrites=true&w=majority`
-mongoose.connect(url)
-const noteSchema = new mongoose.Schema({
-    content: String,
-    date: Date,
-    important: Boolean,
-})
-// hide id & v
-noteSchema.set('toJSON', {
-    transform: (document, returnedObject) => {
-        returnedObject.id = returnedObject._id.toString()
-        delete returnedObject._id
-        delete returnedObject.__v
-    }
-})
-const Note = mongoose.model('Note', noteSchema)
-
-const generateId = () => {
-    const maxId = notes.length > 0 ? Math.max(...notes.map(n => n.id)) : 0
-    return maxId + 1
-}
 
 // define API
 app.post('/api/notes', (request, response) => {
     const body = request.body
 
-    if (!body.content) {
+    if (body.content === undefined) {
         return response.status(400).json({
             error: 'content missing'
         })
     }
 
-    const note = {
+    const note = new Note({
         content: body.content,
         important: body.important || false,
         date: new Date(),
-        id: generateId(),
-    }
+    })
 
-    notes = notes.concat(note)
-
-    response.json(note)
+    note.save().then(savedNote => {
+        console.log('savedNote', savedNote)
+        response.json(savedNote)
+    })
 })
-
 app.get('/', (request, response) => {
     response.send('<h1>Hello World</h1>')
 })
-
 app.get('/api/notes', (request, response) => {
     Note.find({}).then(notes => {
         console.log(notes)
@@ -78,16 +56,10 @@ app.get('/api/notes', (request, response) => {
     })
 })
 app.get('/api/notes/:id', (request, response) => {
-    const id = Number(request.params.id)
-    const note = notes.find(note => note.id === id)
-
-    if (note) {
+    Note.findById(request.params.id).then(note => {
         response.json(note)
-    } else {
-        response.status(404).end()
-    }
+    })
 })
-
 app.delete('/api/notes/:id', (request, response) => {
     const id = Number(request.params.id)
     notes = notes.filter(n => n.id !== id)
@@ -95,8 +67,10 @@ app.delete('/api/notes/:id', (request, response) => {
     response.status(204).end()
 })
 
+// middleware for unknown url
 app.use(unknownEndpoint)
-const PORT = 3001
+
+
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)
 })
---
+++ models/note.js
@@ -0,0 +1,30 @@
+// config mongo db
+const mongoose = require('mongoose')
+const url = process.env.MONGODB_URI //`mongodb+srv://sece1024:sece@cluster0.5mrpqe5.mongodb.net/noteApp?retryWrites=true&w=majority`
+
+console.log('connecting to', url)
+
+mongoose.connect(url)
+    .then(result => {
+        console.log('connected to MongoDB')
+    })
+    .catch((error) => {
+        console.log('error connecting to MongoDB:', error.message)
+    })
+
+const noteSchema = new mongoose.Schema({
+    content: String,
+    date: Date,
+    important: Boolean,
+})
+// hide id & v
+noteSchema.set('toJSON', {
+    transform: (document, returnedObject) => {
+        returnedObject.id = returnedObject._id.toString()
+        delete returnedObject._id
+        delete returnedObject.__v
+    }
+})
+const Note = mongoose.model('Note', noteSchema)
+
+module.exports = mongoose.model('Note', noteSchema)
