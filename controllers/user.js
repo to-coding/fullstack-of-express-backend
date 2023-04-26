398a3d30060b210f91577e7597df85a60f531d79
gaoxiang.huang <hgx1233626@163.com>
2023-04-26 17:24:10 +0800

    test: check user name unique

--- controllers/user.js
+++ controllers/user.js
@@ -5,6 +5,13 @@ const User = require('../models/user')
 usersRouter.post('/', async (request, response) => {
     const { username, name, password } = request.body
 
+    const existingUser = await User.findOne({ username })
+    if(existingUser){
+        return response.status(400).json({
+            error: 'username must be unique'
+        })
+    }
+
     const saltRounds = 10
     const passwordHash = await bcrypt.hash(password, saltRounds)
 
--- tests/user_api.test.js
+++ tests/user_api.test.js
@@ -36,4 +36,25 @@ describe('when there is initially one user in db', () => {
         const usernames = usersAtEnd.map(u => u.username)
         expect(usernames).toContain(newUser.username)
     }, 100000)
+
+    test('creation fails with proper statuscode and message if username already taken', async () => {
+        const usersAtStart = await helper.usersInDb()
+
+        const newUser = {
+            username: 'root',
+            name: 'Matti Luukka',
+            password: 'salainen',
+        }
+
+        const result = await api
+            .post('/api/users')
+            .send(newUser)
+            .expect(400)
+            .expect('Content-Type', /application\/json/)
+
+        expect(result.body.error).toContain('username must be unique')
+
+        const usersAtEnd = await helper.usersInDb()
+        expect(usersAtEnd).toEqual(usersAtStart)
+    })
 })
