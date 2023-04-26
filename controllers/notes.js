aff9e1e81ad54e0144e577751a709a91c619027f
gaoxiang.huang <hgx1233626@163.com>
2023-04-26 17:55:09 +0800

    feat: add note with user info

--- controllers/notes.js
+++ controllers/notes.js
@@ -1,17 +1,24 @@
 const notesRouter = require('express').Router()
+const User = require('../models/user')
 const Note = require('../models/note')
 
 // define API
 notesRouter.post('/', async (request, response) => {
     const body = request.body
 
+    const user = await User.findById(body.userId)
+
     const note = new Note({
         content: body.content,
         important: body.important || false,
         date: new Date(),
+        user: user._id
     })
 
     const savedNote = await note.save()
+    user.notes = user.notes.concat(savedNote._id)
+    await user.save()
+
     response.status(201).json(savedNote)
 
 })
