e27cb82ef451af3e40193bdd4737c70d3499a438
gaoxiang.huang <hgx1233626@163.com>
2023-04-26 15:30:08 +0800

    refactor: use express-async-errors eliminating

--- controllers/notes.js
+++ controllers/notes.js
@@ -2,7 +2,7 @@ const notesRouter = require('express').Router()
 const Note = require('../models/note')
 
 // define API
-notesRouter.post('/', async (request, response, next) => {
+notesRouter.post('/', async (request, response) => {
     const body = request.body
 
     const note = new Note({
@@ -11,36 +11,25 @@ notesRouter.post('/', async (request, response, next) => {
         date: new Date(),
     })
 
-    try {
-        const savedNote = await note.save()
-        response.status(201).json(savedNote)
-    } catch(exception){
-        next(exception)
-    }
+    const savedNote = await note.save()
+    response.status(201).json(savedNote)
+
 })
 notesRouter.get('/', async (request, response) => {
     const notes = await Note.find({})
     response.json(notes)
 })
-notesRouter.get('/:id', async (request, response, next) => {
-    try{
-        const note = await Note.findById(request.params.id)
-        if(note){
-            response.json(note)
-        } else {
-            response.status(404).end()
-        }
-    } catch(exception){
-        next(exception)
+notesRouter.get('/:id', async (request, response) => {
+    const note = await Note.findById(request.params.id)
+    if(note){
+        response.json(note)
+    } else {
+        response.status(404).end()
     }
 })
-notesRouter.delete('/:id', async (request, response, next) => {
-    try{
-        await Note.findByIdAndRemove(request.params.id)
-        response.status(204).end()
-    } catch(exception) {
-        next(exception)
-    }
+notesRouter.delete('/:id', async (request, response) => {
+    await Note.findByIdAndRemove(request.params.id)
+    response.status(204).end()
 })
 notesRouter.put('/:id', (request, response, next) => {
     const body = request.body
