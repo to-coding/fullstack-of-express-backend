bb516f6ad2bf9ebc6167eae6290f55c0e39a4973
gaoxiang.huang <hgx1233626@163.com>
2023-04-26 10:28:50 +0800

    feat: add logger

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,41 +1,41 @@
 module.exports = {
-  'env': {
-    'node': true,
-    'browser': true,
-    'commonjs': true,
-    'es2021': true
-  },
-  'extends': 'eslint:recommended',
-  'overrides': [
-  ],
-  'parserOptions': {
-    'ecmaVersion': 'latest'
-  },
-  'rules': {
-    'eqeqeq': 'error',
-    'no-trailing-spaces': 'error',
-    'object-curly-spacing': [
-      'error', 'always'
+    'env': {
+        'node': true,
+        'browser': true,
+        'commonjs': true,
+        'es2021': true
+    },
+    'extends': 'eslint:recommended',
+    'overrides': [
     ],
-    'arrow-spacing': [
-      'error', { 'before': true, 'after': true }
-    ],
-    'indent': [
-      'error',
-      2
-    ],
-    'linebreak-style': [
-      'error',
-      'unix'
-    ],
-    'quotes': [
-      'error',
-      'single'
-    ],
-    'semi': [
-      'error',
-      'never'
-    ],
-    'no-console': 0
-  }
+    'parserOptions': {
+        'ecmaVersion': 'latest'
+    },
+    'rules': {
+        'eqeqeq': 'error',
+        'no-trailing-spaces': 'error',
+        'object-curly-spacing': [
+            'error', 'always'
+        ],
+        'arrow-spacing': [
+            'error', { 'before': true, 'after': true }
+        ],
+        'indent': [
+            'error',
+            4
+        ],
+        'linebreak-style': [
+            'error',
+            'unix'
+        ],
+        'quotes': [
+            'error',
+            'single'
+        ],
+        'semi': [
+            'error',
+            'never'
+        ],
+        'no-console': 0
+    }
 }
